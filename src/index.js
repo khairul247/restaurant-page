d159c9cf79200d039abcb3d75d216cea36ec3321
Muhammad Khairul Ikhwan <khairulikhwan@Muhammads-MacBook-Air.local>
2024-07-18 18:19:09 +0800

    modify

--- src/index.js
+++ src/index.js
@@ -3,12 +3,5 @@ console.log('Webpack is working!');
 import loadHomePage from './home';
 import './style.css';
 
-document.addEventListener('DOMContentLoaded', () => {
-    loadHomePage();
+loadHomePage();
 
-    document.getElementById('home').addEventListener('click', loadHomePage);
-    document.getElementById('menu').addEventListener('click', () => {
-        const content = document.getElementById('content');
-        content.innerHTML = '<h1>Menu</h1><p>Our delicious menu will be listed here.</p>';
-    });
-});
